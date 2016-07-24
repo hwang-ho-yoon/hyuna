8edde44f0fcc7122f81969b1d5782795b856abf3
내문서 <@180.233.238.95>
2016-07-24 22:56:23 +0900

    이메일 ㅇㅇ

--- hyuna/src/com/hyuna/email/EmailMember.java
+++ hyuna/src/com/hyuna/email/EmailMember.java
@@ -10,7 +10,44 @@ import com.hyuna.vo.MemberVO;
 public class EmailMember {
 String resultMsg;
 	
-	public void setSendEmail(MemberVO mvo){
+public void setSendEmail(MemberVO mvo){
+	long beginTime = System.currentTimeMillis();
+	
+	SimpleEmail simpleEmail = new SimpleEmail();
+	//smtp 서버 연결 설정.(구글 아이디, 비번)
+	simpleEmail.setHostName("smtp.gmail.com");
+	simpleEmail.setSmtpPort(587);
+	simpleEmail.setAuthentication("whdals414", "tlqkaqk1");
+	String rt = "Failure";
+	
+	try {		
+		//smtp SSL,TLS설정
+		simpleEmail.setSSL(true);
+		simpleEmail.setTLS(true);
+		
+		//받는 사람설정
+		simpleEmail.addTo(mvo.getMem_mail(), mvo.getMem_name(), "utf-8");
+		//보내는 사람설정
+		simpleEmail.setFrom("whdals414@gamil.com", "현아", "utf-8");
+		//제목설정
+		simpleEmail.setSubject("비밀번호 테스트");
+		//본문설정
+		simpleEmail.setMsg(mvo.getMem_name()+" 님의 가입을 환영합니다.");
+		rt = simpleEmail.send();
+		resultMsg = "success";
+		
+	}catch(EmailException e){
+		e.printStackTrace();
+		resultMsg = "failure";
+	}
+	finally {
+		long execTime = System.currentTimeMillis() - beginTime;
+		System.err.println("exec time:"+execTime+"ms");
+		System.err.println("RT msg:"+rt);
+	}
+	
+}
+	public void setPwdEmail(MemberVO mvo){
 		long beginTime = System.currentTimeMillis();
 		
 		SimpleEmail simpleEmail = new SimpleEmail();
