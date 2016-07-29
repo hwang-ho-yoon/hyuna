9e21f5d64af3186238d1dbd5adcd39b103202b84
hoyoon <hoyoon@hoyoon-PC>
2016-07-29 23:19:31 +0900

    local

--- hyuna/src/com/hyuna/service/admin/AdminServiceImpl.java
+++ hyuna/src/com/hyuna/service/admin/AdminServiceImpl.java
@@ -8,25 +8,38 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.hyuna.dao.admin.AdminDao;
+import com.hyuna.vo.AdminVO;
 import com.hyuna.vo.MemberVO;
+import com.hyuna.vo.TermsVO;
+import com.hyuna.vo.Terms_agreeVO;
 @Service
 @Transactional
 public class AdminServiceImpl implements AdminService{
 	Logger logger = Logger.getLogger(AdminServiceImpl.class);
 
 	@Autowired
-	private AdminDao admindDao;
+	private AdminDao adminDao;
 
 	@Override
-	public List<MemberVO> memberList() {
+	public List<MemberVO> memberList(MemberVO mvo) {
 		List<MemberVO> list = null;
-		list = admindDao.memberList();
+		list = adminDao.memberList(mvo);
 		return list;
 	}
 
 	@Override
-	public MemberVO memberDetail(MemberVO mvo) {		
-		return admindDao.memberDetail(mvo);
+	public List<Terms_agreeVO> memberDetail(MemberVO mvo) {		
+		return adminDao.memberDetail(mvo);
+	}
+
+	@Override
+	public AdminVO adminCheck(AdminVO avo) {		
+		return adminDao.adminCheck(avo);
+	}
+
+	@Override
+	public int memberListCnt(MemberVO mvo) {		
+		return adminDao.memberListCnt(mvo);
 	}
 
 }
